def0ad26cf7c46799251d1952b980122a52a22e3
atharvagadade22 <168889152+atharvagadade22@users.noreply.github.com>
2024-11-07 17:55:42 +0530

    Add files via upload
    Uploaded the basic files before adding any database functionality.

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, jsonify
+
+
+app = Flask(__name__)
+@app.route("/")
+def HomePage():
+    return render_template("home.html")
+
+
+if(__name__ == "__main__"):
+    app.run("0.0.0.0",5000,debug= True)
